b68f30f9bda8437e0db36521936a6fba79a0e1eb
noso <nas779@usask.ca>
2026-05-05 11:45:14 -0400

    added radialSearch to tree and membership check, not tested yet

--- KdTree.f90
+++ KdTree.f90
@@ -4,6 +4,9 @@ module KdTree
 
     include "node/node.inc"
     include "tree/tree.inc"
+
+    integer, save :: nextTreeId = 0 !> saves tree ids for node membership checks
+
     contains
         include "node/methods.inc"
         include "tree/methods.inc"
